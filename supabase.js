f8e572a2d9c6772f6c8977f48f2bd1e80f105843
Muhammad Abdulrehman <{muhammadabdulrehman513@gmail.com}>
2025-03-06 12:46:14 -0500

    supa_base_back_end

--- supabase.js
+++ supabase.js
@@ -1,15 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
 
-dotenv.config();  // This loads environment variables from .env
+const supabaseUrl = "https://gmtnsgvgydsfdvyyywyy.supabase.co";
+const supabaseAnonKey = "dragonsconnect/process.env";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-if (supabaseUrl != supabaseKey) {
-  throw new Error('Supabase URL or Key is missing.');
-}
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
